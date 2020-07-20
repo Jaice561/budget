bd09c5e50d33123061f08d8af133ff7305f25d46
Jaice561 <Jameson_exil@yahoo.com>
2020-07-20 17:19:27 -0500

    Deploy to Heroku

--- src/pages/BudgetDetail/BudgetDetail.jsx
+++ src/pages/BudgetDetail/BudgetDetail.jsx
@@ -1,12 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {Bar} from 'react-chartjs-2';
 
-class BudgetDetail extends Component {
+const state = {
+  labels: ["jan", "feb", "mar", "fri"],
+  datasets: [
+    {
+      label: 'Monthly Graph',
+      backgroundColor: 'rgba(75,192,192,1)',
+      borderColor: 'rgba(0,0,0,1)',
+      borderWidth: 2,
+      data: [1, 2, 3, 4]
+    }
+  ]
+}
+export default class BudgetDetail extends React.Component {
     render() {
         return (
             <>
+             <div>
+                       <Bar
+                         data={state}
+                         options={{
+                           title:{
+                             display:true,
+                             text:'Month',
+                             fontSize:20
+                           },
+                           legend:{
+                             display:true,
+                             position:'right'
+                           }
+                         }}
+                       />
+                     </div>
             </>
         )
     }
 }
 
-export default BudgetDetail;
--- src/pages/BudgetListPage/BudgetListPage.jsx
+++ src/pages/BudgetListPage/BudgetListPage.jsx
@@ -1,22 +1,7 @@
 import React from 'react';
 import BudgetCollection from '../../components/BudgetCollection/BudgetCollection';
 import './BudgetListPage.css';
-import {Bar} from 'react-chartjs-2';
 
-const state = {
-  labels: ["jan", "feb", "mar", "fri"],
-  datasets: [
-    {
-      label: 'Monthly Graph',
-      backgroundColor: 'rgba(75,192,192,1)',
-      borderColor: 'rgba(0,0,0,1)',
-      borderWidth: 2,
-      data: [1, 2, 3, 4]
-    }
-  ]
-}
-
-       
 function BudgetListPage(props) {
     return (
         <>
@@ -29,22 +14,7 @@ function BudgetListPage(props) {
                     budget={budget}
                     handleDeleteBudget={props.handleDeleteBudget}
                     />
-                    <div>
-                       <Bar
-                         data={state}
-                         options={{
-                           title:{
-                             display:true,
-                             text:'Month',
-                             fontSize:20
-                           },
-                           legend:{
-                             display:true,
-                             position:'right'
-                           }
-                         }}
-                       />
-                     </div>
+                   
                      </>
                 )}
         </div>
